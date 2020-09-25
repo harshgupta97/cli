56615aa61df889513aaae06bb355072fb83d1183
Harsh Gupta <warmachine476@gmail.com>
2020-09-25 12:42:17 +0530

    adding a credential prompt using inquirer

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 
+const inquirer = require('./lib/inquirer');
+
 clear();
 
 console.log(chalk.yellow(figlet.textSync('Parallel', { horizontalLayout: 'full' })));
+
+const run = async () => {
+    const credentails = await inquirer.askCredentials();
+    console.log(credentails);
+};
+
+run();
