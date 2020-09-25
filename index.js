8ed1eee7f90618c9a04b46236752de00762c80f9
Harsh Gupta <warmachine476@gmail.com>
2020-09-25 12:30:26 +0530

    added the first console response Parallel

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+clear();
+
+console.log(chalk.yellow(figlet.textSync('Parallel', { horizontalLayout: 'full' })));
